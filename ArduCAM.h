e4144d26055453c82a627a0499ceb1276bd3adfb
Lena Voytek <dvoytek@email.arizona.edu>
2020-04-21 07:14:18 -0700

    Did heavy library cleaning

--- ArduCAM.h
+++ ArduCAM.h
@@ -6,41 +6,6 @@
   ArduCAM.com. You can find the latest version of the library at
   http://www.ArduCAM.com
 
-  Now supported controllers:
-		- OV7670
-		- MT9D111
-		- OV7675
-		- OV2640
-		- OV3640
-		- OV5642
-		- OV5640
-		- OV7660
-		- OV7725
-		- MT9M112		
-		- MT9V111
-		- OV5640		
-		- MT9M001			
-		- MT9T112
-		- MT9D112
-				
-	We will add support for many other sensors in next release.
-	
-  Supported MCU platform
- 		-	Theoretically support all Arduino families
-  		-	Arduino UNO R3			(Tested)
-  		-	Arduino MEGA2560 R3		(Tested)
-  		-	Arduino Leonardo R3		(Tested)
-  		-	Arduino Nano			(Tested)
-  		-	Arduino DUE				(Tested)
-  		- Arduino Yun				(Tested)  		
-  		-	Raspberry Pi			(Tested)
-  		- ESP8266-12				(Tested)  		
-		* Feather M0                (Tested with OV5642)
-
-  If you make any modifications or improvements to the code, I would appreciate
-  that you share the code with me so that I might include it in the next release.
-  I can be contacted through http://www.ArduCAM.com
-
   This library is free software; you can redistribute it and/or
   modify it under the terms of the GNU Lesser General Public
   License as published by the Free Software Foundation; either
@@ -55,48 +20,6 @@
   License along with this library; if not, write to the Free Software
   Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 */
-
-/*------------------------------------
-	Revision History:
-	2012/09/20 	V1.0.0	by Lee	first release 
-	2012/10/23  V1.0.1  by Lee  Resolved some timing issue for the Read/Write Register	
-	2012/11/29	V1.1.0	by Lee  Add support for MT9D111 sensor
-	2012/12/13	V1.2.0	by Lee	Add support for OV7675 sensor
-	2012/12/28  V1.3.0	by Lee	Add support for OV2640,OV3640,OV5642 sensors
-	2013/02/26	V2.0.0	by Lee	New Rev.B shield hardware, add support for FIFO control 
-															and support Mega1280/2560 boards 
-	2013/05/28	V2.1.0	by Lee	Add support all drawing functions derived from UTFT library 			
-	2013/08/24	V3.0.0	by Lee	Support ArudCAM shield Rev.C hardware, features SPI interface and low power mode.
-								Support almost all series of Arduino boards including DUE.	
-	2014/03/09  V3.1.0  by Lee  Add the more impressive example sketches. 
-								Optimise the OV5642 settings, improve image quality.
-								Add live preview before JPEG capture.
-								Add play back photos one by one	after BMP capture.
-	2014/05/01  V3.1.1  by Lee  Minor changes to add support Arduino IDE for linux distributions.	
-	2014/09/30  V3.2.0  by Lee  Improvement on OV5642 camera dirver.			
-	2014/10/06  V3.3.0  by Lee  Add OV7660,OV7725 camera support.			
-	2015/02/27  V3.4.0  by Lee  Add the support for Arduino Yun board, update the latest UTFT library for ArduCAM.			
-	2015/06/09  V3.4.1  by Lee	Minor changes and add some comments		
-	2015/06/19  V3.4.2  by Lee	Add support for MT9M112 camera.			
-	2015/06/20  V3.4.3  by Lee	Add support for MT9V111 camera.			
-	2015/06/22  V3.4.4  by Lee	Add support for OV5640 camera.										
-	2015/06/22  V3.4.5  by Lee	Add support for MT9M001 camera.		
-	2015/08/05  V3.4.6  by Lee	Add support for MT9T112 camera.	
-	2015/08/08  V3.4.7  by Lee	Add support for MT9D112 camera.							
-	2015/09/20  V3.4.8  by Lee	Add support for ESP8266 processor.	
-	2016/02/03	V3.4.9	by Lee	Add support for Arduino ZERO board.
-	2016/06/07  V3.5.0  by Lee	Add support for OV5642_CAM_BIT_ROTATION_FIXED.
-	2016/06/14  V3.5.1  by Lee	Add support for ArduCAM-Mini-5MP-Plus OV5640_CAM.	
-	2016/09/29	V3.5.2	by Lee	Optimize the OV5642 register settings		
-	2016/10/05	V4.0.0	by Lee	Add support for second generation of ArduCAM shield V2, ArduCAM-Mini-5MP-Plus(OV5642/OV5640).				
-	2016/10/28  V4.0.1  by Lee	Add support for Raspberry Pi
-	2017/04/27  V4.1.0  by Lee	Add support for OV2640/OV5640/OV5642 functions.
-	2017/07/07  V4.1.0  by Lee	Add support for ArduCAM_ESP32 paltform
-	2017/07/25  V4.1.1  by Lee	Add support for MT9V034
-	2017/11/27  V4.1.2  by Max      Add support for Feather M0
-	2018/10/15  V4.1.2  by Lee      Add support for NRF52
-	2018/10/15  V4.1.2  by Lee      Add support for TEENSYDUINO
---------------------------------------*/
 
 #ifndef ArduCAM_H
 #define ArduCAM_H
@@ -115,8 +38,6 @@
 #define cport(port, data) port &= data
 #define sport(port, data) port |= data
 
-#define swap(type, i, j) {type t = i; i = j; j = t;}
-
 #define fontbyte(x) cfont.font[x]  
 
 #define regtype volatile uint32_t
@@ -125,11 +46,11 @@
 
 
 /****************************************************/
-/* Sensor related definition 												*/
+/* Sensor related definition 						*/
 /****************************************************/
 #define BMP 	0
 #define JPEG	1
-#define RAW	  2
+#define RAW		2
 
 #define OV7670		0	
 #define MT9D111_A	1
@@ -149,7 +70,7 @@
 #define MT9T112		15
 #define MT9D112		16
 #define MT9V034 	17
-#define MT9M034   18
+#define MT9M034		18
 
 #define OV2640_160x120 		0	//160x120
 #define OV2640_176x144 		1	//176x144
@@ -169,7 +90,7 @@
 #define OV3640_640x480		3	//640x480
 #define OV3640_800x600 		4	//800x600
 #define OV3640_1024x768		5 //1024x768
-#define OV3640_1280x960	  6	//1280x960
+#define OV3640_1280x960		6	//1280x960
 #define OV3640_1600x1200	7	//1600x1200
 #define OV3640_2048x1536	8	//2048x1536
 
@@ -181,15 +102,15 @@
 #define OV5642_1600x1200	4	//1600x1200
 #define OV5642_2048x1536	5	//2048x1536
 #define OV5642_2592x1944	6	//2592x1944
-#define OV5642_1920x1080  7
+#define OV5642_1920x1080	7
 
 
 #define OV5640_320x240 		0	//320x240 
 #define OV5640_352x288		1	//352x288
-#define OV5640_640x480 	  2	//640x480
-#define OV5640_800x480	  3	//800x480
-#define OV5640_1024x768	  4	//1024x768
-#define OV5640_1280x960	  5	//1280x960	
+#define OV5640_640x480		2	//640x480
+#define OV5640_800x480		3	//800x480
+#define OV5640_1024x768		4	//1024x768
+#define OV5640_1280x960		5	//1280x960	
 #define OV5640_1600x1200	6	 //1600x1200
 #define OV5640_2048x1536	7  //2048x1536
 #define OV5640_2592x1944	8	 //2592x1944
@@ -307,15 +228,15 @@
 
 
 
-#define Sharpness1                         0
-#define Sharpness2                         1
-#define Sharpness3                         2
-#define Sharpness4                         3
-#define Sharpness5                         4
-#define Sharpness6                         5
-#define Sharpness7                         6
-#define Sharpness8                         7
-#define Sharpness_auto                       8
+#define Sharpness1		0
+#define Sharpness2		1
+#define Sharpness3		2
+#define Sharpness4		3
+#define Sharpness5		4
+#define Sharpness6		5
+#define Sharpness7		6
+#define Sharpness8		7
+#define Sharpness_auto	8
 
 
 
@@ -354,7 +275,7 @@
 #define Auto_Detection                 3
 
 /****************************************************/
-/* I2C Control Definition 													*/
+/* I2C Control Definition 							*/
 /****************************************************/
 #define I2C_ADDR_8BIT 0
 #define I2C_ADDR_16BIT 1
@@ -371,17 +292,11 @@
 #define SENSOR_VAL_TERM_8BIT                0xFF
 #define SENSOR_VAL_TERM_16BIT               0xFFFF
 
-//Define maximum frame buffer size
-#if (defined OV2640_MINI_2MP)
-#define MAX_FIFO_SIZE		0x5FFFF			//384KByte
-#elif (defined OV5642_MINI_5MP || defined OV5642_MINI_5MP_BIT_ROTATION_FIXED || defined ARDUCAM_SHIELD_REVC)
-#define MAX_FIFO_SIZE		0x7FFFF			//512KByte
-#else
 #define MAX_FIFO_SIZE		0x7FFFFF		//8MByte
-#endif 
+
 
 /****************************************************/
-/* ArduChip registers definition 											*/
+/* ArduChip registers definition 					*/
 /****************************************************/
 #define RWBIT									0x80  //READ AND WRITE BIT IS BIT[7]
 
@@ -401,8 +316,6 @@
 #define VSYNC_LEVEL_MASK   		0x02  //0 = High active , 		1 = Low active
 #define LCD_BKEN_MASK      		0x04  //0 = Enable, 					1 = Disable
 #define PCLK_DELAY_MASK  		0x08  //0 = data no delay,		1 = data delayed one PCLK
-//#define FIFO_PWRDN_MASK	   		0x20  	//0 = Normal operation, 1 = FIFO power down
-//#define LOW_POWER_MODE			  0x40  	//0 = Normal mode, 			1 = Low power mode
 
 #define ARDUCHIP_FIFO      		0x04  //FIFO and I2C control
 #define FIFO_CLEAR_MASK    		0x01
@@ -453,128 +366,81 @@ struct sensor_reg {
 
 class ArduCAM 
 {
-	public:
+public:
 	ArduCAM( void );
 	ArduCAM(byte model ,int CS);
 	void InitCAM( void );
-	
+
 	void CS_HIGH(void);
 	void CS_LOW(void);
-	
+
 	void flush_fifo(void);
 	void start_capture(void);
 	void clear_fifo_flag(void);
 	uint8_t read_fifo(void);
-	
+
 	uint8_t read_reg(uint8_t addr);
 	void write_reg(uint8_t addr, uint8_t data);	
-	
+
 	uint32_t read_fifo_length(void);
 	void set_fifo_burst(void);
-	
+
 	void set_bit(uint8_t addr, uint8_t bit);
 	void clear_bit(uint8_t addr, uint8_t bit);
 	uint8_t get_bit(uint8_t addr, uint8_t bit);
 	void set_mode(uint8_t mode);
- 
-  uint8_t bus_write(int address, int value);
+
+	uint8_t bus_write(int address, int value);
 	uint8_t bus_read(int address);	
- 
+
 	// Write 8 bit values to 8 bit register address
 	int wrSensorRegs8_8(const struct sensor_reg*);
-	
+
 	// Write 16 bit values to 8 bit register address
 	int wrSensorRegs8_16(const struct sensor_reg*);
-	
+
 	// Write 8 bit values to 16 bit register address
 	int wrSensorRegs16_8(const struct sensor_reg*);
-	
-  // Write 16 bit values to 16 bit register address
+
+	// Write 16 bit values to 16 bit register address
 	int wrSensorRegs16_16(const struct sensor_reg*);
-	
+
 	// Read/write 8 bit value to/from 8 bit register address	
 	byte wrSensorReg8_8(int regID, int regDat);
 	byte rdSensorReg8_8(uint8_t regID, uint8_t* regDat);
-	
+
 	// Read/write 16 bit value to/from 8 bit register address
 	byte wrSensorReg8_16(int regID, int regDat);
 	byte rdSensorReg8_16(uint8_t regID, uint16_t* regDat);
-	
+
 	// Read/write 8 bit value to/from 16 bit register address
 	byte wrSensorReg16_8(int regID, int regDat);
 	byte rdSensorReg16_8(uint16_t regID, uint8_t* regDat);
-	
+
 	// Read/write 16 bit value to/from 16 bit register address
 	byte wrSensorReg16_16(int regID, int regDat);
 	byte rdSensorReg16_16(uint16_t regID, uint16_t* regDat);
 
-	void OV2640_set_JPEG_size(uint8_t size);
-	void OV3640_set_JPEG_size(uint8_t size);
 	void OV5642_set_JPEG_size(uint8_t size);
-	void OV5640_set_JPEG_size(uint8_t size);
-	
 	void OV5642_set_RAW_size (uint8_t size);
-	
-	
-	void OV2640_set_Light_Mode(uint8_t Light_Mode);
-  void OV3640_set_Light_Mode(uint8_t Light_Mode);
 	void OV5642_set_Light_Mode(uint8_t Light_Mode);
-	void OV5640_set_Light_Mode(uint8_t Light_Mode);
-	
-	void OV2640_set_Color_Saturation(uint8_t Color_Saturation);
-	void OV3640_set_Color_Saturation(uint8_t Color_Saturation);
 	void OV5642_set_Color_Saturation(uint8_t Color_Saturation);
-	void OV5640_set_Color_Saturation(uint8_t Color_Saturation);
-	
-	
-	void OV2640_set_Brightness(uint8_t Brightness);
-	void OV3640_set_Brightness(uint8_t Brightness);
-  void OV5642_set_Brightness(uint8_t Brightness);
-  void OV5640_set_Brightness(uint8_t Brightness);
-	
-	void OV2640_set_Contrast(uint8_t Contrast);
-	void OV3640_set_Contrast(uint8_t Contrast);
+	void OV5642_set_Brightness(uint8_t Brightness);
 	void OV5642_set_Contrast(uint8_t Contrast);
-	void OV5640_set_Contrast(uint8_t Contrast);
-	
-	void OV2640_set_Special_effects(uint8_t Special_effect);
-	void OV3640_set_Special_effects(uint8_t Special_effect);
 	void OV5642_set_Special_effects(uint8_t Special_effect);
-	void OV5640_set_Special_effects(uint8_t Special_effect);
-	
-	
-	void OV3640_set_Exposure_level(uint8_t level);
-	void OV3640_set_Sharpness(uint8_t Sharpness);
-	void OV3640_set_Mirror_Flip(uint8_t Mirror_Flip);
-	
-	
 	void OV5642_set_hue(uint8_t degree);
 	void OV5642_set_Exposure_level(uint8_t level);
 	void OV5642_set_Sharpness(uint8_t Sharpness);
-  void OV5642_set_Mirror_Flip(uint8_t Mirror_Flip);
-  void OV5642_set_Compress_quality(uint8_t quality);
-  void OV5642_Test_Pattern(uint8_t Pattern);
-   
-  
-  void OV5640_set_EV(uint8_t EV);
-  void OV5640_set_Night_Mode(uint8_t Night_mode);
-  void OV5640_set_Banding_Filter(uint8_t Banding_Filter);
-	
-	
-	
-	
-	void set_format(byte fmt);
-	
-	#if defined (RASPBERRY_PI)
-    uint8_t transfer(uint8_t data);
-	void transfers(uint8_t *buf, uint32_t size);
-    #endif
+	void OV5642_set_Mirror_Flip(uint8_t Mirror_Flip);
+	void OV5642_set_Compress_quality(uint8_t quality);
+	void OV5642_Test_Pattern(uint8_t Pattern);
 
+	void set_format(byte fmt);
 	void transferBytes_(uint8_t * out, uint8_t * in, uint8_t size);
 	void transferBytes(uint8_t * out, uint8_t * in, uint32_t size);
 	inline void setDataBits(uint16_t bits);
-	
-  protected:
+		
+protected:
 	regtype *P_CS;
 	regsize B_CS;
 	byte m_fmt;
